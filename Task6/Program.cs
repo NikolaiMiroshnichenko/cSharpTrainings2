54312aeefe49e08fe3e11eb63b993403873a4f4f
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2019-09-26 19:22:08 +0300

    Task 06

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -10,6 +10,39 @@ namespace Task6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter some calendar day");
+            if (int.TryParse(Console.ReadLine(), out int day) && day < 32 && day > 0)
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("You enter wrong day");
+            }
+            Console.WriteLine("Enter some calendar month");
+            if (int.TryParse(Console.ReadLine(), out int month) && month < 13 && month > 0)
+            {
+            }
+            else
+            {
+                Console.WriteLine("You enter wrong month");
+            }
+            Console.WriteLine("Enter some year");
+            if (int.TryParse(Console.ReadLine(), out int year) && day < 10000 && day > 0)
+            {
+            }
+            else
+            {
+                Console.WriteLine("You enter wrong year");
+            }
+
+            string someStringData = ($"{day}.{month}.{year}");
+            DateTime someData = new DateTime();
+            someData = Convert.ToDateTime(someStringData);
+            Console.WriteLine(someData.ToLongDateString());
+
+            
+            Console.ReadKey();
         }
     }
 }
