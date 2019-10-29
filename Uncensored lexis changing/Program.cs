6f6fab18c3068c8f78b104ccef540cb1e6de9b9a
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2019-10-29 19:18:23 +0300

    1

--- Uncensored lexis changing/Program.cs
+++ Uncensored lexis changing/Program.cs
@@ -13,7 +13,6 @@ namespace Uncensored_lexis_changing
         {
             Console.WriteLine("Enter some string for uncensored lexis cheking");
             string s1 = Console.ReadLine();
-            /*string pattern = @"(\w*fuck\w*)(\^*ass)(\^*cunt)(\w*faggot\w*)(\w*shit\w*)(\w*bullshit\w*)";*/
             string pattern = @"\w*fuck\w*|\w*ass\w*|\\w*cunt\w*|\w*faggot\w*|\w*shit\w*|\w*bullshit";
             Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
             string s2 = Regex.Replace(s1, pattern, "\"censored\"");
